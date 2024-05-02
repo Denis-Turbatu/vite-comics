6417fa9de424b41fd88cef252bcbcd129e7d47fe
Denis-Turbatu <turbatudenis81@gmail.com>
2024-05-02 15:02:10 +0200

    feat: vue components and scss files

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+
+    },
+}
+</script>
+
+<template>
+    <header>
+        <img :src="imgPath(image)" alt="logo-dc">
+        
+        <nav>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss">
+    
+</style>
