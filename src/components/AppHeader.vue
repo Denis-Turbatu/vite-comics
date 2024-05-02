1297e19eeb6d97d0552c1344ce511f320a04244d
Denis-Turbatu <turbatudenis81@gmail.com>
2024-05-02 15:36:31 +0200

    feat: header component with img and navbar links

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,24 +2,95 @@
 export default {
     data() {
         return {
-            
+            menu:[
+                {
+                    title: "CHARACTERS",
+                    isActive: false,
+                },
+                {
+                    title: "COMICS",
+                    isActive: true,
+                },
+                {
+                    title: "MOVIES",
+                    isActive: false,
+                },
+                {
+                    title: "TV",
+                    isActive: false,
+                },
+                {
+                    title: "GAMES",
+                    isActive: false,
+                },
+                {
+                    title: "COLLECTIBLES",
+                    isActive: false,
+                },
+                {
+                    title: "VIDEOS",
+                    isActive: false,
+                },
+                {
+                    title: "FANS",
+                    isActive: false,
+                },
+                {
+                    title: "NEWS",
+                    isActive: false,
+                },
+                {
+                    title: "SHOP",
+                    isActive: false,
+                },
+            ]
         }
     },
     methods: {
-
+        imgPath: function(){
+            return new URL(`../assets/img/dc-logo.png`, import.meta.url).href
+        }
     },
 }
 </script>
 
 <template>
-    <header>
-        <img :src="imgPath(image)" alt="logo-dc">
-        
-        <nav>
-        </nav>
-    </header>
+    <div class="container">
+        <header>
+            <a href="#">
+                <img :src="imgPath()" alt="logo-dc">
+            </a>
+
+            <nav>
+                <a
+                    href=""
+                    v-for="item,index in menu"
+                    :class="{active: item.isActive}"
+                >
+                    {{menu[index].title}}
+                </a>
+            </nav>
+        </header>
+    </div>
+
 </template>
 
-<style lang="scss">
-    
+<style lang="scss" scoped>
+    @use "../style/partials/mixin" as *;
+
+    .container{
+        @include container(1180px, auto);
+        header{
+        @include flex(row, space-between, center);
+        padding: 15px 0;
+        nav {
+            display: flex;
+            gap: 1.6rem;
+            a{
+                color: #1c1c1c;
+                text-decoration: none;
+            }
+        }
+    }
+    }
 </style>
